dd3fffd4d774c73a7914287da1f4c9a4588752bd
Max <lomanuy@gmail.com>
2022-10-04 20:44:33 +0300

    complete

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int[] array = new int[5];
+
+void GetArray()
+{
+    int count = 1;
+    int i = 0;
+    while (array.Length > i)
+    {
+        Console.WriteLine($"Enter the {count} element for the array: ");
+        int arrayElement = Convert.ToInt32(Console.ReadLine());
+        array[i] = arrayElement;
+        i++;
+        count++;
+    }
+}
+GetArray();
+
+void PrintArray()
+{
+    int i = 0;
+    while (array.Length > i)
+    {
+        Console.Write($"|{array[i]}|");
+        i++;
+    }
+    return;
+}
+PrintArray();
+
+void EvenCount()
+{
+    int count = 0;
+    int i = 0;
+    while (array.Length > i)
+    {
+        if (array[i] > 0) count++;
+        i++;
+    }
+    Console.WriteLine();
+    Console.WriteLine(count);
+    return;
+}
+
+EvenCount();
--- task_43/Program.cs
+++ task_43/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите координаты точки b1: "); 
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+CrossPoint(k1,b1,k2,b2);
+
+void CrossPoint(double k1, double b1, double k2, double b2)
+ { 
+    double x = (b1-b2)/(k2-k1);
+    double y = (k2*b1-k1*b2)/(k2-k1);
+if (k1 == k2)
+ Console.Write("Прямые не пересекаются");
+ if (k1 == k2 && b1 == b2)
+ Console.Write(". Прямые совпадают.");
+    else Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+ }  
